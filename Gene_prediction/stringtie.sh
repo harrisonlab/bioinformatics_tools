4a08b20bfb6f45d2fa5967f0b294a24dc7c71890
agomezcortecero <antonio.gomez@niab.com>
2020-05-19 19:31:08 +0100

    stringtie assembler of RNA-Seq alignments

---
+++ Gene_prediction/stringtie.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#SBATCH -J stringtie
+#SBATCH --partition=long
+#SBATCH --mem-per-cpu=1G
+#SBATCH --cpus-per-task=16
+
+# Aligned transcripts assembly using a reference genome.
+
+CurDir=$PWD
+WorkDir=$PWD/${SLURM_JOB_USER}_${SLURM_JOBID}
+
+AcceptedHits=$1
+OutDir=$2
+
+Usage="stringtie.sh <Aligned_reads.bam> <Out_directory>"
+
+echo "$Usage"
+echo "Aligned reads: $AcceptedHits"
+echo "Output directory: $OutDir"
+
+mkdir -p $WorkDir/out
+cd $WorkDir
+mkdir -p $CurDir/$OutDir
+
+cp $CurDir/$AcceptedHits accepted_hits.bam
+
+stringtie -o out.gtf -C cov_refs.gtf -p 8 accepted_hits.bam
+
+sed -i 1,2d out.gtf
+
+cp -r $WorkDir/* $CurDir/$OutDir/.
+rm -r $WorkDir
