00a3732a0d39737f623eb3a4800c7d7983d3c06c
Antonio Gómez Cortecero <antonio.gomez@niab.com>
2020-06-02 22:28:17 +0100

    errors fixed

--- Feature_annotation/interproscan.sh
+++ Feature_annotation/interproscan.sh
@@ -13,18 +13,18 @@ SplitDir=gene_pred/interproscan/$Organism/$Strain
 mkdir -p $SplitDir
 $InterproDir/splitfile_500.py --inp_fasta $InFile --out_dir $SplitDir --out_base "$InName"_split
 
-#for file in $(ls $SplitDir/Inter_split_*); do
-#sbatch $InterproDir/run_interproscan.sh $file
-#done
-
-for file in $(ls $SplitDir/*_split*); do
-        Jobs=$(squeue | grep 'interpro' | grep 'qw' | wc -l)
-        while [ $Jobs -gt 1 ]; do
-                sleep 10
-                printf "."
-                Jobs=$(squeue | grep 'interproscan' | grep 'qw' | wc -l)
-        done
-        printf "\n"
-        echo $file
-        sbatch $InterproDir/run_interproscan.sh $file
+for file in $(ls $SplitDir/*_split_*); do
+sbatch $InterproDir/run_interproscan.sh $file
 done
+
+#for file in $(ls $SplitDir/*_split*); do
+        #Jobs=$(squeue | grep 'interpro' | grep 'qw' | wc -l)
+        #while [ $Jobs -gt 1 ]; do
+                #sleep 10
+                #printf "."
+                #Jobs=$(squeue | grep 'interproscan' | grep 'qw' | wc -l)
+        #done
+        #printf "\n"
+        #echo $file
+        #sbatch $InterproDir/run_interproscan.sh $file
+#done
