f7219aabf1b6d0baa108684f37b42bca50632209
Antonio Gómez Cortecero <antonio.gomez@niab.com>
2020-06-11 21:42:32 +0100

    WorkDir edited

--- Feature_annotation/run_interproscan.sh
+++ Feature_annotation/run_interproscan.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH -J interproscan
-#SBATCH --partition=medium
-#SBATCH --mem-per-cpu=2G
-#SBATCH --cpus-per-task=4
+#SBATCH --partition=short
+#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=8
 
 # run_interproscan.sh
 # The current version of interproscan only works with Java version 11
@@ -16,7 +16,7 @@ ORGANISM=$(echo $IN_FILE | rev | cut -d "/" -f3 | rev)
 STRAIN=$(echo $IN_FILE | rev | cut -d "/" -f2 | rev)
 IN_NAME=$(basename $IN_FILE)
 
-WORK_DIR=$TMPDIR/interproscan
+WORK_DIR=$PWD/${SLURM_JOB_USER}_${SLURM_JOBID}
 
 mkdir -p $WORK_DIR
 cd $WORK_DIR
