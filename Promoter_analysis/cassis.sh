40dfdd29325c161f48584520f48b78dc71ede4b8
Antonio Gómez Cortecero <antonio.gomez@niab.com>
2020-06-29 22:24:40 +0100

    promoter-based secmet prediction

---
+++ Promoter_analysis/cassis.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#SBATCH -J cassis
+#SBATCH --partition=short
+#SBATCH --mem=4G
+#SBATCH --cpus-per-task=12
+
+
+
+Assembly=$1
+CassisTSV=$2
+GeneID=$3
+OutDir=$4
+
+cassis \
+  --annotation $CassisTSV \
+  --genome $Assembly \
+  --anchor $GeneID \
+  --dir $OutDir/$GeneID \
+  --mismatches 0 \
+  -v \
+  --prediction \
+  --num-cpus 4 \
+  | tee 2>&1 $OutDir/$GeneID/${GeneID}_log.txt
