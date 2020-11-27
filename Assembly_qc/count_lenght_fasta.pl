730be637533d9fea0b96aed1f9b467474abc6c41
agomezcortecero <antonio.gomez@niab.com>
2020-11-27 15:33:25 +0000

    Calculate length of each contig

---
+++ Assembly_qc/count_lenght_fasta.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl -w
+
+use strict;
+#contig_length_fasta.pl
+my $largest = 0;
+
+my $contig = '';
+
+if (@ARGV != 1) {
+        print "contig_length_fastq fasta\n\n" ;
+        exit ;
+}
+
+my $filenameA = $ARGV[0];
+
+open (IN, "$filenameA") or die "oops!\n" ;
+
+        my $read_name = '' ;
+        my $read_seq = '' ;
+
+        while (<IN>) {
+            if (/^>(\S+)/) {
+                $read_name = $1 ;
+                $read_seq = "" ;
+
+                while (<IN>) {
+
+                        if (/^>(\S+)/) {
+
+                            print "$read_name\t" . length($read_seq) . "\n" ;
+
+                            $read_name = $1 ;
+                            $read_seq = "" ;
+
+                        }
+
+                        else {
+                            chomp ;
+                            $read_seq .= $_ ;
+                        }
+
+                }
+
+            }
+        }
+
+close(IN) ;
+
+print "$read_name\t" . length($read_seq) . "\n" ;
