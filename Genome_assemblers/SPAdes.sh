f3a073ba138e3e080cd989b6467273875650533f
agomezcortecero <antonio.gomez@niab.com>
2021-03-31 18:58:50 +0100

    new tools

---
+++ Genome_assemblers/SPAdes.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#Assemble contigs using SPAdes
+
+#!/usr/bin/env bash
+#SBATCH -J SPAdes
+#SBATCH --partition=himem
+#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=16
+
+Usage="SPAdes.sh <F_read.fa> <R_read.fa> <output_directory> <correct/only-assembler> [<coverage_cutoff>]"
+echo "$Usage"
+
+R1=$1
+R2=$2
+OutDir=$3
+Correction=$4
+Cutoff='auto'
+if [ $5 ]; then
+  Cutoff=$5
+fi
+
+CurPath=$PWD
+WorkDir=$TMPDIR/${SLURM_JOB_USER}_${SLURM_JOBID}
+
+F_Read=$(basename $R1)
+R_Read=$(basename $R2)
+
+cp $CurPath/$R1 $WorkDir/$F_Read
+cp $CurPath/$R2 $WorkDir/$R_Read
+
+
+echo  "Running SPADES with the following in='$R1 $R2' $OutDir "
+echo "You have set read correction to: $Correction"
+echo "Coverage cutoff set to $Cutoff"
+
+if [[ "$Correction" == 'correct' ]]; then
+  spades.py -k 21,33,55,77,99,127 -m 94 --phred-offset 33 --careful -1 $WorkDir/$F_Read -2 $WorkDir/$R_Read -t 16  -o $WorkDir/. --cov-cutoff "$Cutoff"
+elif [[ "$Correction" == 'only-assembler' ]]; then
+  spades.py -k 21,33,55,77,99,127 -m 94 --phred-offset 33 --careful --only-assembler -1 $WorkDir/$F_Read -2 $WorkDir/$R_Read -t 16  -o $WorkDir/. --cov-cutoff "$Cutoff"
+else
+  echo "Please set fourth option - whether you require read correction [correct / only-assembler]"
+  exit
+fi
+
+#echo "Filtering contigs smaller than 500bp"
+#mkdir -p $WorkDir/filtered_contigs
+#FilterDir=/home/armita/git_repos/emr_repos/tools/seq_tools/assemblers/abyss
+#$FilterDir/filter_abyss_contigs.py $WorkDir/scaffolds.fasta 500 > $WorkDir/filtered_contigs/contigs_min_500bp.fasta
+
+
+rm $WorkDir/$F_Read
+rm $WorkDir/$R_Read
+mkdir -p $CurPath/$OutDir
+cp -r $WorkDir/* $CurPath/$OutDir/.
+echo "files copied"
---
+++ Genome_assemblers/quickmerge.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+#SBATCH -J quickmerge
+#SBATCH --partition=himem
+#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=16
+
+# Merge a Pacbio only (Canu) and a hybrid pacbio-illumina (Spades) assembly
+# to make a merged assembly.
+# Submit the best assembly as the first input and the worse of the two Assemblies
+# as the second input. The first input will be used to scaffold.
+# The N50 is recomended to be used as the Anchor length for contigs. However,
+# beware that if the N50 is very large (>1Mb) then problems may be encountered
+# if trying to scaffold small chromosomes (<1Mb).
+
+Usage="quickmerge.sh <better_assembly.fa> <worse_assembly.fa> <output_directory>"
+echo "$Usage"
+
+# ---------------
+# Step 1
+# Collect inputs
+# ---------------
+
+PacbioFa=$1
+HybridFa=$2
+OutDir=$3
+Prefix=pacbio_hybrid_merged
+AnchorOverlapCuttoff=5.0
+ExtensionOverlapCuttoff=1.5
+AnchorContigLength=$4
+MinAlignmentLength=5000
+
+echo "Running Quickmerge with the following inputs:"
+echo "Pacbio assembly (or best quality assembly) - $PacbioFa"
+echo "Hybrid assembly (or worse assembly) - $HybridFa"
+echo "OutDir - $OutDir"
+echo "Contigs of this size will be used as anchors for merging - $AnchorContigLength"
+
+# ---------------
+# Step 2
+# Copy data
+# ---------------
+
+CurPath=$PWD
+WorkDir="$TMPDIR"/quickmerge
+mkdir -p $WorkDir
+cd $WorkDir
+cp $CurPath/$PacbioFa pacbio.fa
+cp $CurPath/$HybridFa hybrid.fa
+
+# ---------------
+# Step 3
+# Run quickmerge
+# ---------------
+
+merge_wrapper.py \
+  pacbio.fa \
+  hybrid.fa \
+  --prefix pacbio_hybrid_merged \
+  -hco $AnchorOverlapCuttoff \
+  -c $ExtensionOverlapCuttoff \
+  --length_cutoff $AnchorContigLength \
+  --length_minimum $MinAlignmentLength \
+  > merge_report.txt
+
+rm pacbio.fa
+rm hybrid.fa
+
+mkdir -p $CurPath/$OutDir
+# mv merged.fasta $CurPath/$OutDir/.
+# mv merge_report.txt $CurPath/$OutDir/.
+cp -r $WorkDir/* $CurPath/$OutDir/.
+
+rm -r $WorkDir
