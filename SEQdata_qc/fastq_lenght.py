9e91c631acf4a16dd754f97bddb9b7479e90d9cd
agomezcortecero <antonio.gomez@niab.com>
2021-04-30 13:39:01 +0100

    calculate reads length

---
+++ SEQdata_qc/fastq_lenght.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+######################################################################################
+syntax = '''
+------------------------------------------------------------------------------------
+Usage: python fastq_lenght.py file.fastq 
+result: .txt file same name as input name plus "_lenghts.txt" 
+------------------------------------------------------------------------------------
+'''
+######################################################################################
+
+if len(sys.argv) != 2:
+    print syntax
+    sys.exit()
+
+######################################################################################
+
+fastq_file = open(sys.argv[1], 'r')
+prefix = sys.argv[1].split('.')[0]
+outfile = open(prefix + '_' + 'lenghts.txt', 'w')
+seq = ''
+for line in fastq_file:
+    line = line.rstrip('\n')
+    if line.startswith('@'):
+        if seq:
+            outfile.write(name + '\t' + str(len(seq)) + '\n')
+            seq = ""
+        name = line 
+    else:
+        seq = line 
+outfile.write(name + '\t' + str(len(seq)) + '\n')
+fastq_file.close()
+outfile.close()
+
+print '\n' + '\t' + 'File: ' + prefix + '_' + 'lenghts.txt has been created...'
