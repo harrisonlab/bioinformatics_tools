1e9e270aad3d2d02434b1f75018b91dfd9c49424
agomezcortecero <antonio.gomez@niab.com>
2021-05-17 12:06:19 +0100

    output edited

--- SNP_calling/GenotypeGVCFs.sh
+++ SNP_calling/GenotypeGVCFs.sh
@@ -11,7 +11,7 @@ Reference=$2
 OutDir=$3
 
 filename=$(basename "$Reference")
-output=$input/"${filename%.*}_genotyped.vcf.gz"
+output="${filename%.*}_genotyped.vcf.gz"
 #output2=$input/"${filename%.*}.vcf"
 
 /scratch/software/gatk4/gatk-4.2.0.0/gatk GenotypeGVCFs \
