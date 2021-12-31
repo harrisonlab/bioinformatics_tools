fa51415b62c2310b88f9728f734e12c2ae3c7cf3
agomezcortecero <antonio.gomez@niab.com>
2021-12-31 17:37:52 +0100

    filter fasta by gene ID

---
+++ Miscellaneous/filter_fasta_by_list_of_headers.py
@@ -0,0 +1,16 @@
+#!/home/gomeza/miniconda3/envs/gene_pred/bin/python
+
+from Bio import SeqIO
+import sys
+
+ffile = SeqIO.parse(sys.argv[1], "fasta")
+header_set = set(line.strip() for line in open(sys.argv[2]))
+
+for seq_record in ffile:
+    try:
+        header_set.remove(seq_record.name)
+    except KeyError:
+        print(seq_record.format("fasta"))
+        continue
+if len(header_set) != 0:
+    print(len(header_set),'of the headers from list were not identified in the input fasta file.', file=sys.stderr)
