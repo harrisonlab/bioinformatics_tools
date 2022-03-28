afca822c53e09890c628907d107511bdb1f67231
agomezcortecero <antonio.gomez@niab.com>
2022-03-28 15:33:40 +0100

    adapted for gruff

--- Gene_prediction/cufflinks.sh
+++ Gene_prediction/cufflinks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH -J cufflinks
-#SBATCH --partition=medium
+#SBATCH --partition=long
 #SBATCH --mem-per-cpu=6G
 #SBATCH --cpus-per-task=40
 
@@ -25,7 +25,7 @@ mkdir -p $CurDir/$OutDir
 
 cp $CurDir/$AcceptedHits accepted_hits.bam
 
-cufflinks -o out -p 16 --max-intron-length 4000 accepted_hits.bam 2>&1 | tee out/cufflinks.log
+/home/agomez/scratch/apps/prog/cufflinks/cufflinks-2.2.1.Linux_x86_64/cufflinks -o out -p 16 --max-intron-length 4000 accepted_hits.bam 2>&1 | tee out/cufflinks.log
 
 rm accepted_hits.bam
 cp -r $WorkDir/out/* $CurDir/$OutDir/.
